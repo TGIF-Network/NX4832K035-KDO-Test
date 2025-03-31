31624e1b4ab0362bb4f314f450cf2cb4a3ca0290
root <root@pi-star>
2025-03-31 13:31:55 -0400

    Update Security Page

--- updatepiconfig.sh
+++ updatepiconfig.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 ##############################################################
-# create basic wpa_supplicant  				     #
+# Script to activate Accept Changes in Pi-Star Config	     #
 # in /home/pi-star/ directory                                #
 #                                                            #
 #                                                            #
-# VE3RD                                         2025-03-24   #
+# VE3RD                                         2025-03-31   #
 ##############################################################
 
+sudo mount -o remount,rw /
+
+#if [ -z "$1" ]; then
+#  echo "syntax:   updatepi.sh <pi-star ssid or address> <password> "
+#  exit
+#fi
+
+decode_string() {
+    local input="$1"
+    echo -n "$input" | base64 -d
+}
+
+encrypted=$(cat /etc/pspass)
+
+decrypted=$(decode_string "$encrypted")
+#echo "$decrypted"
 
 
-if [ -z "$1" ]; then
-  echo "syntax:   updatepi.sh <pi-star ssid or address> <password> "
-  exit
-fi
+pw="$decrypted"
 
-piAddress=$1
-pw=$2
+piAddress=$(hostname)
 
-curl -X POST http://"$piAddress"/admin/configure.php  -u "pi-star:$pw" -d "action=save" -d "config=1"
+curl -X POST http://"$piAddress"/admin/configure.php  -u "pi-star:$pw" -d "action=save" -d "config=1" &> /dev/null
+
 
 status=$?
 
