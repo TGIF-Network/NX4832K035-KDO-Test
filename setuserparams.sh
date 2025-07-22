6cedc63c2a7da189abdb4fc8c235b24893e076a0
root <root@pi-star>
2025-07-22 09:48:48 -0400

    updated

--- setuserparams.sh
+++ setuserparams.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 ############################################################
-#  Activate  Profile                                       #
-#  VE3RD                                      2019/11/18   #
+#  Set User Parameters                                     #
+#  VE3RD                                      2025-07-22   #
 ############################################################
 set -o errexit
 set -o pipefail
 ver="20250204"
 dirn=/etc/mmdvmhost
-declare -i pnum
-args=("$@")
 
-sudo mount -o remount,rw /
-systemctl stop mmdvmhost
 
 if [ -z "$4" ]; then
-        exit
+   echo "No Frequency Provided"     
+   exit
 fi
+
+	systemctl stop mmdvmhost
+
 	sudo mount -o remount,rw /
 
         sudo sed -i '/^\[/h;G;/General/s/\(^Callsign=\).*/\1'"$1"'/m;P;d' /etc/mmdvmhost
         sudo sed -i '/^\[/h;G;/General/s/\(^Id=\).*/\1'"$2"'/m;P;d' /etc/mmdvmhost
+        sudo sed -i '/^\[/h;G;/DMR/s/\(^WhiteList=\).*/\1'"$2"'/m;P;d' /etc/mmdvmhost
 
 	DMRID="$2""$5"
-        sudo sed -i '/^\[/h;G;/General/s/\(^Id=\).*/\1'"$DMRID"'/m;P;d' /etc/mmdvmhost
-
-
-        sudo sed -i '/^\[/h;G;/DMR Network/s/\(^Password=\).*/\1'"$3"'/m;P;d' /etc/mmdvmhost
-
+        sudo sed -i '/^\[/h;G;/DMR/s/\(^Id=\).*/\1'"$DMRID"'/m;P;d' /etc/mmdvmhost
 
         sudo sed -i '/^\[/h;G;/DMR Network/s/\(^Password=\).*/\1'"$3"'/m;P;d' /etc/mmdvmhost
+ #       sudo sed -i '/^\[/h;G;/DMR Network/s/\(^Password=\).*/\1'"$3"'/m;P;d' /etc/mmdvmhost
 
 
-        sudo sed -i '/^\[/h;G;/Info/s/\(^RXFrequency=\).*/\1'"$m4"'/m;P;d' /etc/mmdvmhost
-        sudo sed -i '/^\[/h;G;/Info/s/\(^TXFrequency=\).*/\1'"$m4"'/m;P;d' /etc/mmdvmhost
+        sudo sed -i '/^\[/h;G;/Info/s/\(^RXFrequency=\).*/\1'"$4"'/m;P;d' /etc/mmdvmhost
+        sudo sed -i '/^\[/h;G;/Info/s/\(^TXFrequency=\).*/\1'"$4"'/m;P;d' /etc/mmdvmhost
 
 	
 
